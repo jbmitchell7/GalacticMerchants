63d186c40394fa3f9df698ce30dbd8aaad7ad1bd
jbmitchell7 <jbmitchell747@gmail.com>
2022-02-22 21:26:06 -0600

    trying another api tutorial

--- app/src/main/java/cloud/jakemitchell/galacticmerchants/MainActivity.kt
+++ app/src/main/java/cloud/jakemitchell/galacticmerchants/MainActivity.kt
@@ -2,10 +2,22 @@ package cloud.jakemitchell.galacticmerchants
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import cloud.jakemitchell.galacticmerchants.network.RetrofitHelper
+import cloud.jakemitchell.galacticmerchants.network.SpaceTradersApiService
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val statusApi = RetrofitHelper.getInstance().create(SpaceTradersApiService::class.java)
+        GlobalScope.launch {
+            val result = statusApi.getGameStatus()
+                if (result != null)
+                    Log.d("Game Status: ", result.toString())
+        }
     }
 }
--- app/src/main/java/cloud/jakemitchell/galacticmerchants/network/SpaceTradersApiService.kt
+++ app/src/main/java/cloud/jakemitchell/galacticmerchants/network/SpaceTradersApiService.kt
@@ -1,28 +1,9 @@
 package cloud.jakemitchell.galacticmerchants.network
 
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
-private const val API_URL = "https://api.spacetraders.io"
-
-private val moshi = Moshi.Builder()
-    .add(KotlinJsonAdapterFactory())
-    .build()
-
-private val retrofit = Retrofit.Builder()
-    .addConverterFactory(MoshiConverterFactory.create(moshi))
-    .baseUrl(API_URL)
-    .build()
 
 interface SpaceTradersApiService {
     @GET("/game/status")
     suspend fun getGameStatus(): String
-}
-
-object SpaceTradersApi {
-    val retrofitService : SpaceTradersApiService by lazy {
-        retrofit.create(SpaceTradersApiService::class.java) }
 }
