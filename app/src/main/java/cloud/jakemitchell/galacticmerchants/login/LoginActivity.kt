4c198ee2466a0cb05ff99a07cd2411cfd9f9500c
jbmitchell7 <jbmitchell747@gmail.com>
2022-03-05 20:02:28 -0600

    refactored to make loginactivity into a viewmodel instead of global scope

---
+++ app/src/main/java/cloud/jakemitchell/galacticmerchants/login/LoginActivity.kt
@@ -0,0 +1,37 @@
+package cloud.jakemitchell.galacticmerchants.login
+
+import android.os.Bundle
+import android.content.Context
+import androidx.activity.viewModels
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import cloud.jakemitchell.galacticmerchants.R
+import cloud.jakemitchell.galacticmerchants.network.data.GameStatus
+
+class LoginActivity : AppCompatActivity() {
+    private val model: LoginViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val statusObserver = Observer<GameStatus> { gameStatus ->
+            val loginBtn = findViewById<Button>(R.id.login)
+            val tokenText = findViewById<EditText>(R.id.access_token)
+            val userText = findViewById<EditText>(R.id.crew_name)
+            val pref = getPreferences(Context.MODE_PRIVATE)
+            val edit = pref.edit()
+            loginBtn.setOnClickListener {
+                //will remove eventually
+                loginBtn.text = gameStatus.status
+                edit.putString("TOKEN", tokenText.text.toString())
+                edit.putString("USER", userText.text.toString())
+                edit.apply()
+            }
+        }
+        model.gameStatus.observe(this, statusObserver)
+    }
+
+}
---
+++ app/src/main/java/cloud/jakemitchell/galacticmerchants/login/LoginViewModel.kt
@@ -0,0 +1,32 @@
+package cloud.jakemitchell.galacticmerchants.login
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import cloud.jakemitchell.galacticmerchants.network.SpaceTradersApi
+import cloud.jakemitchell.galacticmerchants.network.data.GameStatus
+import kotlinx.coroutines.launch
+import java.lang.Exception
+
+class LoginViewModel : ViewModel() {
+
+    private val _gameStatus = MutableLiveData<GameStatus>()
+
+    val gameStatus: LiveData<GameStatus> = _gameStatus
+
+    init {
+        getStatus()
+    }
+
+    private fun getStatus() {
+        viewModelScope.launch {
+            try {
+                val result = SpaceTradersApi.retrofitService.getGameStatus()
+                _gameStatus.value = result
+            } catch (e: Exception) {
+                println("Failed")
+            }
+        }
+    }
+}
