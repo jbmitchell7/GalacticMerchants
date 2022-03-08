cef8f17f89b063ee86b427235473c0fd9a8b8fe4
jbmitchell7 <mitc0380@morris.umn.edu>
2022-03-08 11:24:51 -0600

    fixed some theme issues and changed how game status is displayed

--- app/src/main/java/cloud/jakemitchell/galacticmerchants/login/LoginActivity.kt
+++ app/src/main/java/cloud/jakemitchell/galacticmerchants/login/LoginActivity.kt
@@ -1,11 +1,14 @@
 package cloud.jakemitchell.galacticmerchants.login
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import androidx.activity.viewModels
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import cloud.jakemitchell.galacticmerchants.MainActivity
@@ -15,19 +18,29 @@ import cloud.jakemitchell.galacticmerchants.network.data.GameStatus
 class LoginActivity : AppCompatActivity() {
     private val model: LoginViewModel by viewModels()
 
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         val statusObserver = Observer<GameStatus> { gameStatus ->
-            val loginBtn = findViewById<Button>(R.id.login)
+            //sets game status message on login screen
+            val statusText = findViewById<TextView>(R.id.status_text)
+            if (gameStatus.status == "spacetraders is currently online and available to play") {
+                statusText.text = "Game is Online"
+                statusText.setTextColor(Color.parseColor("#08A045"))
+            } else {
+                statusText.text ="Game is Offline. Please Try Again Later"
+                statusText.setTextColor(Color.parseColor("#bd251a"))
+            }
+            //sets user and token for sharedpreferences
             val tokenText = findViewById<EditText>(R.id.access_token)
             val userText = findViewById<EditText>(R.id.crew_name)
+            //initializes sharedpreferences
             val pref = getPreferences(Context.MODE_PRIVATE)
             val edit = pref.edit()
+            val loginBtn = findViewById<Button>(R.id.login)
             loginBtn.setOnClickListener {
-                //will remove eventually
-                loginBtn.text = gameStatus.status
                 edit.putString("TOKEN", tokenText.text.toString())
                 edit.putString("USER", userText.text.toString())
                 edit.apply()
